50ca7d08cdea5eb2f0e6deb0b54d6a2965a11eeb
massab-hassan112 <massab@hotglue.io>
2025-08-06 00:03:57 +0500

    feat: add dynamic selecting of streams using api version flag (#7)
    * feat: add dynamic selecting of streams using api version flag
    
    * feat: fetch complete products and orders

--- tap_medusa/tap.py
+++ tap_medusa/tap.py
@@ -5,9 +5,7 @@ from typing import List
 from singer_sdk import Stream, Tap
 from singer_sdk import typing as th
 
-from tap_medusa.streams import OrdersStream, ProductsStream, ReturnsStream
-
-STREAM_TYPES = [OrdersStream, ProductsStream, ReturnsStream]
+from tap_medusa.streams_v1 import OrdersStream as OrdersStreamV1, ProductsStream as ProductsStreamV1, ReturnsStream as ReturnsStreamV1
 
 
 class TapMedusa(Tap):
@@ -44,11 +42,23 @@ class TapMedusa(Tap):
             "api_key",
             th.StringType,
         ),
+        th.Property(
+            "medusa_v2",
+            th.BooleanType,
+            default=False,
+            description="Use Medusa v2 API schemas when True, v1 schemas when False",
+        ),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
-        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
+        if self.config.get("medusa_v2", False):
+            from tap_medusa.streams_v2 import OrdersStream as OrdersStreamV2, ProductsStream as ProductsStreamV2, ReturnsStream as ReturnsStreamV2
+            stream_types = [OrdersStreamV2, ProductsStreamV2, ReturnsStreamV2]
+        else:
+            stream_types = [OrdersStreamV1, ProductsStreamV1, ReturnsStreamV1]
+        
+        return [stream_class(tap=self) for stream_class in stream_types]
 
 
 if __name__ == "__main__":
